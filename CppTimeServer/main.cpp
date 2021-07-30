0ed4d5b72219e1e2e2cbe1a7fb943f31d94b0134
Вадим Перфильев <vadimperfilev@gmail.com>
2021-07-30 14:27:55 +0300

    Time server

---
+++ CppTimeServer/main.cpp
@@ -0,0 +1,36 @@
+#include <exception>
+#include <system_error>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string AskTimeServer(void);
+
+class TimeServer {
+public:
+	string GetCurrentTime()
+	{
+		try
+		{
+			last_fetched_time = AskTimeServer();
+		} 
+		catch (system_error& ex)
+		{}
+		return last_fetched_time;
+	}
+private:
+	string last_fetched_time = "00:00:00";
+};
+
+
+int main() {
+	TimeServer ts;
+	try {
+		cout << ts.GetCurrentTime() << endl;
+	}
+	catch (exception& e) {
+		cout << "Exception got: " << e.what() << endl;
+	}
+	return 0;
+}
